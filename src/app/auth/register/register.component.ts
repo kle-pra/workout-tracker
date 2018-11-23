ce8ca1731b4531bcb67531e304f492c045f62f60
kle-pra <klemen.pravdic@gmail.com>
2018-11-24 00:15:28 +0100

    register form gui

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  maxDate;
   constructor() { }
 
   ngOnInit() {
+
+    //must be 18 years old so set max possible picker date to today - 18 years:
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  onRegisterSubmit(form: NgForm) {
+    console.log(form.value);
   }
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatDatepickerModule, MatInputModule, MatNativeDateModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatDatepickerModule, MatInputModule, MatNativeDateModule, MatToolbarModule, MatCardModule, MatTabsModule, MatSidenavModule, } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatDatepickerModule, MatNativeDateModule, MatInputModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatDatepickerModule, MatNativeDateModule, MatInputModule],
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatTabsModule],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatTabsModule],
 })
 export class MaterialModule { }
